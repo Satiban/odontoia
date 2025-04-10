4f034008496334718900eca5ca16d08eb71631dd
Steve Tibán <steve.tiban@espoch.edu.ec>
2025-04-09 22:15:28 -0500

    Add files via upload

---
+++ Extensiones/js/scroll.js
@@ -0,0 +1,6 @@
+(function() {
+    window.addEventListener("scroll", function() {
+        var header = this.document.querySelector("header");
+        header.classList.toggle("sticky", this.window.scrollY >0)
+    })
+})();
